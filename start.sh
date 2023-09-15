fc912ca2d49100350de8de02a8db68cdcadd01fe
kotecat <davidshaldunov@gmail.com>
2023-09-15 20:14:34 +0600

    v 1.1.3 (add statistic a game) (done!)

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 git pull
 
-if ! [ -d /venv/ ]; then
+if ! [ -d /venv ]; then
     echo "CREATE VENV"
     python3 -m pip install virtualenv
     echo "creating venv dir..."
     python3 -m venv venv
     echo "install all requirements..."
-    source venv/bin/activate
+    . venv/bin/activate
     python3 -m pip install -r requirements.txt
 fi
 
 echo "running..."
 
-source venv/bin/activate
+. venv/bin/activate
 
 python3 durak/bot.py
